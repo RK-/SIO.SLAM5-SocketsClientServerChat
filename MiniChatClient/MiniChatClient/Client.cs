78cfd886df64f1996833364fa8bb5aac4cd0ae36
RK <ruby.kevin57@gmail.com>
2014-12-11 15:56:47 +0100

    SIO.SLAM5-SocketsClientServerChat
    TP réalisé en BTS SIO SLAM 5. Utiliser les sockets pour créer un chat
    entre un client et un serveur en C#.

---
+++ MiniChatClient/MiniChatClient/Client.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChatClient
+{
+    public class Client
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        public static void Main(string[] args)
+	    {
+		    TcpClient socketClient;
+		    bool continuer = true;
+            NetworkStream fluxReseau;
+            StreamReader lecteurFlux;
+            StreamWriter ecrivainFlux;
+		
+		    try
+		    {
+			    string IPServer = "127.0.0.1";
+		        // créer le socket pour connexion (TcpClient) avec adresse IP du serveur et numéro de port 50000
+                socketClient = new TcpClient(IPServer, 50000);
+		        // afficher un message "Connecté au serveur " avec son adresse IP
+                Console.WriteLine("Connecté au serveur " + IPServer);
+                // récupérer un flux de données (NetWorkStream)
+                fluxReseau = socketClient.GetStream();
+		        // créer un lecteur de flux (StreamReader) et un écrivain de flux (StreamWriter)
+                lecteurFlux = new StreamReader(fluxReseau);
+                ecrivainFlux = new StreamWriter(fluxReseau);
+		    }
+		    catch (Exception e)
+		    {
+			    Console.WriteLine("Connexion impossible\n" + e.ToString());
+			    Console.ReadLine();
+			    return;
+		    }		
+		    try
+		    {
+			    String messageDuClient = "";
+			    String messageDuServeur = "";
+			
+			    while ( continuer )
+			    {
+				    // lire le message saisi par le client
+                    Console.Write("Client : ");
+                    messageDuClient = Console.ReadLine();
+                    // envoyer le dernier message saisi au serveur
+                    ecrivainFlux.WriteLine(messageDuClient);
+                    ecrivainFlux.Flush();
+				    // si le client a demandé une déconnexion en saisissant "bye" 
+				    if (messageDuClient == "bye")
+				    {
+					    continuer = false;
+				    }
+				    else
+				    {
+					   // lire la réponse du serveur et l'afficher
+                        messageDuServeur = lecteurFlux.ReadLine();
+                        Console.WriteLine("Serveur : " + messageDuServeur);
+				    } // fin du else
+			    } // fin du while
+		    } // fin du try
+		    catch (Exception e)
+		    {
+			    Console.WriteLine("Problème réseau !\n"+ e.ToString());
+			    Console.ReadLine();
+		    }
+			// fermer la connexion
+            ecrivainFlux.Close();
+            lecteurFlux.Close();
+            fluxReseau.Close();
+            socketClient.Close();
+	    }
+    }
+}
---
+++ MiniChatServeur/MiniChatServer/Serveur.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace MiniChatServeur
+{
+    public class Serveur
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        public static void Main(string[] args)
+        {
+           try
+           {
+	        bool continuer = true;
+	        String messageDuClient = "";
+	        String messageDuServeur = "";				
+	        IPAddress adresseIP = IPAddress.Parse("127.0.0.1");
+
+            // créer un socket d'écoute (un écouteur = socketEcouteur) avec adresse IP et numéro de port 50000
+            TcpListener socketEcouteur = new TcpListener(adresseIP, 50000);
+	        // démarrer l'écoute
+            socketEcouteur.Start();
+            Console.WriteLine("Serveur démarré...");
+	        // extraire une connexion pour client(classe TcpClient) reste bloqué ici tant que le client n'est pas connecté
+            TcpClient socketServiceClient = socketEcouteur.AcceptTcpClient();
+	        // afficher "Un client vient de se connecter "  
+            Console.WriteLine("Un client vient de se connecter.\n");
+	        // créer un flux de données réseau (networkStream), 
+            NetworkStream fluxReseau = socketServiceClient.GetStream();
+            // créer un lecteur de flux (StreamReader) 
+            StreamReader lecteurFlux = new StreamReader(fluxReseau);
+            // créer un écrivain de flux (StreamWriter)
+            StreamWriter ecrivainFlux = new StreamWriter(fluxReseau);
+	        // tant que le client ne met pas fin à la connexion
+	        while (continuer)
+	        {
+		        if (socketServiceClient.Connected)
+		        {
+			        // lire et afficher les données envoyées par le client
+                    messageDuClient = lecteurFlux.ReadLine();
+                    Console.WriteLine("Client : " + messageDuClient);
+			        // si le client se déconnecte en saisissant "bye"
+			        if (messageDuClient == "bye")
+			        {   
+                        // arrêter l’écoute 	
+                        continuer = false;
+                    }
+                    else
+			        {  
+                        // lire le message saisi côté serveur
+                        Console.Write("Serveur : ");
+                        messageDuServeur = Console.ReadLine();
+			            // envoyer le message saisi au client  
+                        ecrivainFlux.WriteLine(messageDuServeur);
+                        ecrivainFlux.Flush(); // Forcer l'envoi du message au client
+                    }
+		        }
+	        } // fin du while
+	        // fermer la connexion
+            ecrivainFlux.Close();
+            lecteurFlux.Close();
+            fluxReseau.Close();
+            socketServiceClient.Close();
+	        //fermer l'écouteur
+            socketEcouteur.Stop();
+            Console.WriteLine("Arrêt du serveur");
+            Console.ReadLine();
+           }
+           catch (Exception e )
+           {
+   	        Console.WriteLine(e.ToString());
+            Console.ReadLine();
+           }
+        }// fin du Main
+    }// fin de la classe
+}
